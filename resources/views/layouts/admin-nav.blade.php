d59acd794d9d1a7efad9fdeb798a658b109e56cb
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-27 02:10:09 +0430

    update comment logo in sidebar

--- resources/views/layouts/admin-nav.blade.php
+++ resources/views/layouts/admin-nav.blade.php
@@ -88,7 +88,7 @@ main{
                 <li><a href="#"> <img src="https://svgsilh.com/svg/303194.svg" alt="restaurant-categories-logo" style="width:20px;height:20px"> <span class="mx-1">Restaurant Categories</span></a></li>
                 <li><a href="#"> <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/favorite-food-3144240-2616918.png" alt="food-categorie-logo" style="width:20px;height:20px"> <span class="mx-1">Food Categories</span></a></li>
                 <li><a href="#"> <img src="https://cdn-icons-png.flaticon.com/512/600/600260.png" alt="discounts-logo" style="width:20px;height:20px"> <span class="mx-1">Discounts</span></a></li>
-                <li><a href="#"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Comment_alt_font_awesome.svg/512px-Comment_alt_font_awesome.svg.png" alt="comments-logo" style="width:20px;height:20px"> <span class="mx-1">Comments</span></a></li>
+                <li><a href="#"> <img src="https://cdn.onlinewebfonts.com/svg/img_395479.png" alt="comments-logo" style="width:20px;height:20px"> <span class="mx-1">Comments</span></a></li>
                 <li><a href="#"> <img src="https://static.thenounproject.com/png/4914083-200.png" alt="banner-logo" style="width:20px;height:20px;"> <span class="mx-1">Banners</span></a></li>            </ul>
         </nav>
         <main class="py-4">
