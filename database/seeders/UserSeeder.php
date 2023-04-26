f02a836a9dcecf9287021a34b75d40c30baff6ad
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-26 20:49:33 +0430

    make a admin with seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'phone_number' => rand(10000000000,99999999999),
+            'password' => Hash::make('admin'), // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
