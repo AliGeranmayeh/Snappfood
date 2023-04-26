c260415c1561387feafcda6373decda31e2ce97f
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-26 20:51:28 +0430

    differentiate route for admin after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
@@ -28,6 +29,13 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected function authenticated(Request $request, $user) {
+        if ($user->email == 'admin@admin.com') {
+            return redirect('/admin');
+        } else {
+            return redirect('/');
+        }
+   }
     /**
      * Create a new controller instance.
      *
