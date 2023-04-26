1cc88263c901ba6cd35d7f358980093ee9552e6d
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-27 02:05:33 +0430

    add shadow to cards in admin dashboard

--- resources/views/admin/admin-panel.blade.php
+++ resources/views/admin/admin-panel.blade.php
@@ -2,34 +2,38 @@
 
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-md-center my-5 " style="height: 33%">
-    <div class="col col-lg-10 bg-white p-3">
-      <p>users </p>
-      <a href="{{route('users')}}" class="btn btn-primary">More</a>
-    </div>
+    <div class="container">
+        <div class="row justify-content-md-center my-5 " style="height: 33%">
+            <div class="col col-lg-10 bg-white p-3 rounded"
+            style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
+                <p>users </p>
+                <a href="{{ route('users') }}" class="btn btn-primary">More</a>
+            </div>
+        </div>
+        <div class="row justify-content-md-center my-5" style="height: 33%">
+            <div class="col col-lg-10 bg-white p-3 rounded"
+            style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
+                <p>resturants </p>
+                <a href="#" class="btn btn-primary">More</a>
+            </div>
 
-  </div>
-  <div class="row justify-content-md-center my-5" style="height: 33%">
-    <div class="col col-lg-10 bg-white p-3">
-      <p>resturants </p>
-      <a href="#" class="btn btn-primary">More</a>
+        </div>
+        <div class="row justify-content-md-center" style="height: 33%">
+            <div class="col col-lg-3 bg-white mx-4 p-3 rounded"
+            style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
+                <p>discounts </p>
+                <a href="#" class="btn btn-primary">More</a>
+            </div>
+            <div class="col col-lg-3 bg-white mx-4 p-3 rounded"
+            style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
+                <p>food categories </p>
+                <a href="#" class="btn btn-primary">More</a>
+            </div>
+            <div class="col col-lg-3 bg-white mx-4 p-3 rounded"
+            style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
+                <p>resturant categories</p>
+                <a href="#" class="btn btn-primary">More</a>
+            </div>
+        </div>
     </div>
-
-  </div>
-  <div class="row justify-content-md-center" style="height: 33%">
-    <div class="col col-lg-3 bg-white mx-4 p-3">
-        <p>discounts </p>
-        <a href="#" class="btn btn-primary">More</a>
-    </div>
-    <div class="col col-lg-3 bg-white mx-4 p-3">
-      <p>food categories </p>
-      <a href="#" class="btn btn-primary">More</a>
-    </div>
-    <div class="col col-lg-3 bg-white mx-4 p-3">
-       <p>resturant categories</p>
-      <a href="#" class="btn btn-primary">More</a>
-    </div>
-  </div>
-</div>
 @endsection
