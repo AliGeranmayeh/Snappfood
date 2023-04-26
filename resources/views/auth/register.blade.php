1a658d4745f533fa233fa8af10deff81aa010fad
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-26 19:38:22 +0430

    complete register UI design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
-
+@section('css-link')
+    <link rel="stylesheet" href="styles/auth/auth.css">
+@endsection
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -72,6 +74,64 @@
                 </div>
             </div>
         </div>
+    </div>
+</div> --}}
+<div class="container">
+    @error('email')
+        <div class="alert alert-danger my-3" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('password')
+        <div class="alert alert-danger my-3" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('name')
+        <div class="alert alert-danger my-3" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('phone_number')
+    <div class="alert alert-danger my-3" role="alert">
+        <strong>{{ $message }}</strong>
+    </div>
+@enderror
+    
+    <div class="login-box">
+        <h2>Register</h2>
+        <form>
+            @csrf
+            <div class="user-box">
+                <input type="text" class="@error('name') is-invalid @enderror"  placeholder="Name"
+                     name="name" required>
+                <label>Name</label>
+            </div>
+            <div class="user-box">
+                <input type="tele" class="@error('phone_number') is-invalid @enderror"  placeholder="Phone Number"
+                     name="phone_number" required>
+                <label>Phone Number</label>
+            </div>
+            <div class="user-box">
+                <input type="email" class="@error('email') is-invalid @enderror"  placeholder="Email"
+                     name="email" required>
+                <label>Email</label>
+            </div>
+            <div class="user-box">
+                <input type="password" class="@error('password') is-invalid @enderror" placeholder="Password"
+                    name="password" required>
+                <label>Password</label>
+            </div>
+            <div class="user-box">
+                <input type="password"name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+                <label>Confirm Password</label>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-outline-light">LOGIN</button>
+                <small class="mx-3 text-light">You have an account <a href="login">login here</a></small>
+            </div>
+
+        </form>
     </div>
 </div>
 @endsection
