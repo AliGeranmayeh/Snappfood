054778e660cc91c26818feafb7bea40b7fdd0a20
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 02:47:14 +0430

    add foods model and table

---
+++ app/Models/Food.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'price',
+        'materials',
+        'discount_id',
+        'type_id',
+        'restaurant_id'
+    ];
+}
