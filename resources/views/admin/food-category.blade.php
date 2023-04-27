73853a0cf308007a66ceeb90d7a882e182f8fe2b
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-27 17:23:35 +0430

    add message for empty food category section

--- resources/views/admin/food-category.blade.php
+++ resources/views/admin/food-category.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.admin-nav')
 @section('content')
     <div class="container">
+        @if ($error != null)
+            <div class="alert alert-success my-3" role="alert" style="margin:auto; width: 50%;">
+                <strong class="d-flex justify-content-center">{{$error}}</strong>
+            </div>
+        @endif
         <div class="page rounded-3 bg-white p-3 "
-            style="margin:auto; width: 80%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+            style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
             <h3 class="text-center">Existed Food Categories</h3>
             <div class="my-5">
+                @if (count($food_categories) == 0)
+                <h4 class="text-center">There is no food category...</h4>
+                @endif
                 <table class="table">
                     <tbody>
                         @foreach ($food_categories as $key => $food_category)
@@ -14,13 +22,18 @@
                                     {{ $row_number++ }}
                                 </th>
                                 <td>{{ $food_category->name }}</td>
-                                <form action="" method="post">
-                                    @csrf
-                                    <td><button name="delete" value="{{ $food_category->id }}" type="submit"
-                                            class="btn btn-danger">Delete</button></td>
-                                    <td><button name="edit" value="{{ $food_category->id }}" type="submit"
-                                            class="btn btn-warning">Edit</button></td>
-                                </form>
+                                <td class="d-flex flex-row-reverse">
+                                    <form action="" method="post" class="d-inline p-2">
+                                        @csrf
+                                        <button name="delete" value="{{ $food_category->id }}" type="submit"
+                                            class="btn btn-danger">Delete</button>
+                                    </form>
+                                    <form action="" method="post" class="d-inline p-2">
+                                        @csrf
+                                        <button name="edit" value="{{ $food_category->id }}" type="submit"
+                                            class="btn btn-warning">Edit</button>
+                                    </form>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -28,32 +41,34 @@
             </div>
         </div>
         @if ($name != null)
-            <div class="page rounded-3 bg-white py-3 px-5 my-5"
-                style="margin:auto; width: 80%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+            <div class="page rounded-3 bg-white py-3 px-4 my-5"
+                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
                 <h3 class="text-center">Edit Food Category</h3>
                 <div class="my-5">
                     <form method="post">
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Food Category Name</label>
-                            <input value="{{$name}}" type="text" class="form-control " id="name" name="name">
+                            <input value="{{ $name }}" type="text" class="form-control " id="name"
+                                name="name">
                         </div>
-                        <button value="{{$id}}" name="update" type="submit" class="btn btn-primary my-2">Update</button>
+                        <button value="{{ $id }}" name="update" type="submit"
+                            class="btn btn-primary my-2">Update</button>
                     </form>
                 </div>
             </div>
-            @else
-            <div class="page rounded-3 bg-white py-3 px-5 my-5"
-                style="margin:auto; width: 80%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+        @else
+            <div class="page rounded-3 bg-white py-3 px-4 my-5"
+                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
                 <h3 class="text-center">Add New Food Category</h3>
                 <div class="my-5">
                     <form method="post">
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Food Category Name</label>
-                            <input  type="text" class="form-control " id="name" name="name">
+                            <input type="text" class="form-control " id="name" name="name" required>
                         </div>
-                        <button  name="create" type="submit" class="btn btn-primary my-2">Create</button>
+                        <button name="create" type="submit" class="btn btn-primary my-2">Create</button>
                     </form>
                 </div>
             </div>
