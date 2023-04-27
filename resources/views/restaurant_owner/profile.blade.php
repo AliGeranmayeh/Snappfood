f708b9cdff6919e5e606bf1af16a456594fcbb57
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-27 21:51:12 +0430

    Update profile.blade.php

--- resources/views/restaurant_owner/profile.blade.php
+++ resources/views/restaurant_owner/profile.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger text-center" role="alert" style="margin:auto; width: 80%;">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+
+            </div>
+        @elseif ($error != null)
+            <div style="margin:auto; width: 80%;" class="alert alert-danger text-center" role="alert">
+                <strong>{{ $error }}</strong>
+            </div>
+        @endif
         @if ($restaurant != null)
             <div class="page rounded-3 bg-white p-3 "
                 style="margin:auto; width: 80%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
@@ -9,60 +21,31 @@
                 <div class="my-5">
                     <p style="display: flex;justify-content: center;"
                         class="rounded-pill border border-success border-2 mx-5 ">
-                        <strong>Restaurant Name:</strong> &nbsp;
+                        <strong>Restaurant Name:</strong> &nbsp;{{$restaurant->name}}
                     </p>
                     <p style="display: flex;justify-content: center;"
                         class="rounded-pill border border-success border-2 mx-5 ">
-                        <strong>Restaurant Type:</strong> &nbsp;
+                        <strong>Restaurant Type:</strong> &nbsp;{{$category}}
                     </p>
                     <p style="display: flex;justify-content: center;"
                         class="rounded-pill border border-success border-2 mx-5 ">
-                        <strong>Restaurant Address:</strong>&nbsp;
+                        <strong>Restaurant Address:</strong>&nbsp;{{$restaurant->address}}
                     </p>
                     <p style="display: flex;justify-content: center;"
                         class="rounded-pill border border-success border-2 mx-5 ">
-                        <strong>Restaurant Number:</strong>&nbsp;
+                        <strong>Restaurant Number:</strong>&nbsp;{{$restaurant->phone}}
                     </p>
                     <p style="display: flex;justify-content: center;"
                         class="rounded-pill border border-success border-2 mx-5 ">
-                        <strong>Restaurant Account Number:</strong>&nbsp;
+                        <strong>Restaurant Account Number:</strong>&nbsp;{{$restaurant->account}}
                     </p>
-                </div>
-            </div>
+                    <div  style="display: flex;justify-content: center;">
+                        <a style="width: 90%" href="{{route('edit-restaurant-profile')}}" class="btn btn-success"> Update Your
+                            Restaurant Profile</a>
+                    </div>
 
-            <div class="page rounded-3 bg-white py-3 px-5 my-5"
-                style="margin:auto; width: 80%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-                <h3 class="text-center">Update Restaurant Information</h3>
-                <div class="my-5">
-                    <form method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Restaurant Name</label>
-                            <input type="text" class="form-control " id="name" name="name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="number" class="form-label" style="margin-top: 2%">Restaurant Number</label>
-                            <input type="tele" class="form-control " id="number" name="number">
-                        </div>
-                        <div class="mb-3">
-                            <label for="account" class="form-label" style="margin-top: 2%">Restaurant Account
-                                Number</label>
-                            <input type="text" class="form-control " id="account" name="account">
-                        </div>
-                        <select class="form-select " name="type" style="margin-top: 3%">
-                            <option selected>Select Restaurant Type</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                        <div class="mb-3">
-                            <label for="adress" class="form-label" style="margin-top: 2%">Restaurant Address</label>
-                            <textarea class="form-control " id="address" name="address" rows="3" style=" resize: none; "></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary my-2">Update Restaurant Profile</button>
-                        <small class="mx-3 text-danger">All the users gonna see submited profile</small>
-                    </form>
                 </div>
+
             </div>
         @else
             <div class="page rounded-3 bg-white py-3 px-5 my-5"
@@ -76,25 +59,26 @@
                             <input type="text" class="form-control " id="name" name="name">
                         </div>
                         <div class="mb-3">
-                            <label for="number" class="form-label" style="margin-top: 2%">Restaurant Number</label>
-                            <input type="tele" class="form-control " id="number" name="number">
+                            <label for="number" class="form-label" style="margin-top: 2%">Restaurant Phone</label>
+                            <input type="tele" class="form-control " id="number" name="phone">
                         </div>
                         <div class="mb-3">
                             <label for="account" class="form-label" style="margin-top: 2%">Restaurant Account
                                 Number</label>
                             <input type="text" class="form-control " id="account" name="account">
                         </div>
-                        <select class="form-select " name="type" style="margin-top: 3%">
+                        <select class="form-select " name="type" style="margin-top: 3%" required>
                             <option selected>Select Restaurant Type</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            @foreach ($restaurant_categories as $restaurant_category)
+                                <option value="{{ $restaurant_category->id }}">{{ $restaurant_category->name }}</option>
+                            @endforeach
                         </select>
                         <div class="mb-3">
                             <label for="adress" class="form-label" style="margin-top: 2%">Restaurant Address</label>
                             <textarea class="form-control " id="address" name="address" rows="3" style=" resize: none; "></textarea>
                         </div>
-                        <button name="create" type="submit" class="btn btn-primary my-2">Create Restaurant Profile</button>
+                        <button name="create" type="submit" class="btn btn-primary my-2">Create Restaurant
+                            Profile</button>
                         <small class="mx-3 text-danger">All the users gonna see submited profile</small>
                     </form>
                 </div>
