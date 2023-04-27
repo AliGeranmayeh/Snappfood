c8189b3e37d5bef90a03023e24ea2d7511829527
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-27 20:22:12 +0430

    add index function for get route

--- app/Http/Controllers/Restaurant/ProfileController.php
+++ app/Http/Controllers/Restaurant/ProfileController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Restaurant;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function index()
     {
-        return view('restaurant_owner.profile');
+        $restaurant = User::find(Auth::user()->id)->restaurant;
+        return view('restaurant_owner.profile',[
+            'restaurant' => $restaurant
+        ]);
     }
 }
