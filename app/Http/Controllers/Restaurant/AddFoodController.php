b16dd2fa9e7e580e18d1708534f915ac241ec047
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 03:39:04 +0430

    add add-food route and controller

---
+++ app/Http/Controllers/Restaurant/AddFoodController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Restaurant;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AddFoodController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create(Request $request)
+    {
+        
+    }
+}
