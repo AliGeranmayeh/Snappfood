458bf749a23e16502a4b6f9b17c3ffe591451d9e
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 00:47:57 +0430

    change int to integer

--- database/migrations/2023_04_27_194946_create_discounts_table.php
+++ database/migrations/2023_04_27_194946_create_discounts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->string('name');
-            $table->int('percentage');
+            $table->integer('percentage');
             $table->timestamps();
         });
     }
