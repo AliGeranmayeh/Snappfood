7a1dcf2fbdbd7e6aae82b7831af3a3c2db30aea3
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 14:02:49 +0430

    add Admin and RestaurantOwner middleware

---
+++ app/Http/Middleware/RestaurantOwner.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
+class RestaurantOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user() && Auth::user()->role =='owner') {
+                return $next($request);   
+        }
+        return abort(403);
+    }
+}
