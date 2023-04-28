b885b11eb86f515baa5a125a085fe5f0750aa02a
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 12:06:27 +0430

    add delete,edit,search for home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use App\Models\Food;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Discount;
+use App\Http\Requests\FoodRequest;
+use App\Models\FoodCategory;
 
 class HomeController extends Controller
 {
@@ -27,12 +29,85 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $restaurant_id =User::find(Auth::user()->id)->restaurant->id ;
-        $foods = Food::where('restaurant_id',$restaurant_id)->get();
-        $food_party_id = Discount::where('name' , 'Food Party')->first()->id;
-        return view('restaurant_owner.home',[
-            'foods' =>$foods,
-            'food_party_id' => $food_party_id
+        $food_categories = FoodCategory::all();
+        $restaurant_id = User::find(Auth::user()->id)->restaurant->id;
+        $foods = Food::where('restaurant_id', $restaurant_id)->get();
+        $food_party_id = Discount::where('name', 'Food Party')->first()->id;
+        return view('restaurant_owner.home', [
+            'foods' => $foods,
+            'food_party_id' => $food_party_id,
+            'food_categories' => $food_categories
         ]);
+    }
+
+    public function post(Request $request)
+    {
+        
+        if ($request->has('delete')) {
+           
+            return $this->deleteFood($request);
+        }
+        elseif ($request->has('search')) {
+            
+            return $this->searchFood($request);
+        }
+        $food_categories = FoodCategory::all();
+        $restaurant_id = User::find(Auth::user()->id)->restaurant->id;
+        $foods = Food::where('restaurant_id', $restaurant_id)->get();
+        $food_party_id = Discount::where('name', 'Food Party')->first()->id;
+        return view('restaurant_owner.home', [
+            'foods' => $foods,
+            'food_party_id' => $food_party_id,
+            'food_categories' => $food_categories
+        ]);
+    }
+
+    public function deleteFood($data)
+    {
+        $food = Food::find($data->delete);
+        $image_path = $food->image;
+        if ($image_path != 'images/default.png') {
+            unlink("$image_path");
+        }
+        Food::destroy($data->delete);
+
+        return redirect()->route('home');
+    }
+
+    public function searchFood($data)
+    {
+        
+        $food_categories = FoodCategory::all();
+        $restaurant_id = User::find(Auth::user()->id)->restaurant->id;
+        $food_party_id = Discount::where('name', 'Food Party')->first()->id;
+        if ($data->search_field != null && $data->food_category_filter !=0) {
+            $foods = Food::where('restaurant_id', $restaurant_id)->where('name','like',"%$data->search_field%")->where('type_id',$data->food_category_filter)->get();
+            return view('restaurant_owner.home', [
+                'foods' => $foods,
+                'food_party_id' => $food_party_id,
+                'food_categories' => $food_categories
+            ]);
+        }
+        elseif ($data->search_field != null) {
+            $foods = Food::where('restaurant_id', $restaurant_id)->where('name','like',"%$data->search_field%")->get();
+            // dd($foods);
+            return view('restaurant_owner.home', [
+                'foods' => $foods,
+                'food_party_id' => $food_party_id,
+                'food_categories' => $food_categories
+            ]);
+        }
+        elseif ($data->food_category_filter !=0) {
+            $foods = Food::where('restaurant_id', $restaurant_id)->where('type_id',$data->food_category_filter)->get();
+            return view('restaurant_owner.home', [
+                'foods' => $foods,
+                'food_party_id' => $food_party_id,
+                'food_categories' => $food_categories
+            ]);
+        }
+        else{
+            return redirect()->route('home');
+        }
+        
     }
 }
--- resources/views/restaurant_owner/home.blade.php
+++ resources/views/restaurant_owner/home.blade.php
@@ -26,39 +26,56 @@
 @endsection
 
 @section('content')
+    <form class="my-5" method="post" style="width: 70%;margin:auto">
+        @csrf
+        <select class="form-select d-inline" aria-label="Default select example" style="width: 20%" name="food_category_filter">
+            <option value="0" selected>Food Category Filter</option>
+            @foreach ($food_categories as $food_category)
+                <option value="{{ $food_category->id }}">
+                    {{ $food_category->name }}
+                </option>
+            @endforeach
+        </select>
+        <input class="form-control d-inline mx-2" type="search" placeholder="Search" name="search_field" style="width:70%">
+        <button class="btn btn-outline-light" type="submit" name="search">Search</button>
+    </form>
+    @if (count($foods) == 0)
+            <h2 class="text-center text-white">No food is available...</h2>
+        @endif
     <div class="container grid">
         @foreach ($foods as $food)
-        @if ($food->discount_id == $food_party_id)
-        <article class="rounded p-3 mb-2 bg-secondary text-white">
-            @else
-            <article class="rounded " style="background-color: white">
-        @endif
-                
-                    <img class="rounded-top"
-                        src="{{asset($food->image)}}"
-                        alt="user-photo"
-                        style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;width:100%">
-                    <div class="text ">
-                        <h3 style="margin:10% 0"><b>{{ $food->name }}</b></h3>
-                        <div>
-                            @if ($food->materials)
-                            <p>Materials: {{ $food->materials }}</p>
-                            @endif
-                            @if ($food->discount_id)
-                            <p>Price: <span style="text-decoration: line-through;"> {{ $food->price}}</span> -> with {{$food->discount*100}}% off <span>{{$food->price - ($food->price*$food->discount)}}</span></p>
-                            @else
-                            <p>Price: {{ $food->price }}</p>
-                            @endif
-                            <form action="" method="post">
-                                <button class="btn btn-danger" type="submit" value="{{$food->id}}">Delete</button>
-                                <a  href="{{route('get-edit-food')}}" class="btn btn-warning mx-2">Edit</a>
-                            </form>
-                        </div>
-                        {{-- <a href="user/{{$user->id}}" class="btn btn-primary">More</a> --}}
-                        
-                    </div>
-                </article>
-                
+            @if ($food->discount_id == $food_party_id)
+                <article class="rounded p-3 mb-2 bg-secondary text-white">
+                @else
+                    <article class="rounded " style="background-color: white">
+            @endif
+
+            <img class="rounded-top" src="{{ asset($food->image) }}" alt="user-photo"
+                style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;width:100%">
+            <div class="text ">
+                <h3 style="margin:10% 0"><b>{{ $food->name }}</b></h3>
+                <div>
+                    @if ($food->materials)
+                        <p>Materials: {{ $food->materials }}</p>
+                    @endif
+                    @if ($food->discount_id)
+                        <p>Price: <span style="text-decoration: line-through;"> {{ $food->price }}</span> -> with
+                            {{ $food->discount * 100 }}% off <span>{{ $food->price - $food->price * $food->discount }}</span>
+                        </p>
+                    @else
+                        <p>Price: {{ $food->price }}</p>
+                    @endif
+                    <form action="" method="post">
+                        @csrf
+                        <button name="delete" class="btn btn-danger" type="submit"
+                            value="{{ $food->id }}">Delete</button>
+                        <a href="edit_food/{{ $food->id }}" class="btn btn-warning mx-2">Edit</a>
+                    </form>
+                </div>
+                {{-- <a href="user/{{$user->id}}" class="btn btn-primary">More</a> --}}
+
+            </div>
+            </article>
         @endforeach
     </div>
     </div>
