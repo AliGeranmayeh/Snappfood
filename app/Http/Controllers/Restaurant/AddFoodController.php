b1b90d8f6a3d33eee4b26c71992021a1e2066bfb
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-28 05:05:55 +0430

    complete create method and add default food image

--- app/Http/Controllers/Restaurant/AddFoodController.php
+++ app/Http/Controllers/Restaurant/AddFoodController.php
@@ -28,12 +28,22 @@ class AddFoodController extends Controller
     public function create(FoodRequest $request)
     {
         $request->validated();
+        $image_path = 'images/default.png';
+        $discount_id =null;
+        if($request->has('image')){
+            $new_image_name = time().'-'.$request->name.'.'.$request->image->exetention();
+            $request->image->move(public_path('images'),$new_image_name);
+            $image_path = 'images/'.$new_image_name;
+        }
+        if ($request->discount != 'null') {
+            $discount_id = $request->discount;
+        }
         Food::create([
             'name' => $request->name,
-            'image' => $request->image,
+            'image' => $image_path,
             'price' => $request->price,
             'materials' => $request->materials,
-            'discount_id' => $request->discount,
+            'discount_id' => $discount_id,
             'type_id' => $request->type,
             'restaurant_id' => Restaurant::where('user_id',Auth::user()->id)->first()->id
         ]);
