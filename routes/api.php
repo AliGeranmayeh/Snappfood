0703eafa2fc168a70fea1a066e98f8e57932558d
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-03 16:08:14 +0430

    add route for shopper login and register api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['api'])->group(function ($router) {
+    Route::post('/register' , [AuthController::class, 'register'])->name('shopper-login');
+    Route::post('/login' , [AuthController::class, 'login'])->name('shopper-login');
 });
