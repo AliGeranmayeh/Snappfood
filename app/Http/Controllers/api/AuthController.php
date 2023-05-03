2e649e17cd6ecf43a449fc0519587094e04613a9
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-03 16:08:55 +0430

    add middleware and needed functions for route

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -8,5 +8,17 @@ use Auth;
 
 class AuthController extends Controller
 {
-    //
+
+    public function __construct() {
+        $this->middleware('auth:api' , ['except' => ['login','register']]);
+    }
+    public function register(Request $request)
+    {
+        
+    }
+
+    public function login(Request $request)
+    {
+        
+    }
 }
