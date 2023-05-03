7cb90f0c9d7c71b6c9601cfd5c69661d99f74667
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-03 16:11:22 +0430

    add validation to register function

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use App\Http\Requests\UserRequest;
 
 class AuthController extends Controller
 {
@@ -12,9 +13,9 @@ class AuthController extends Controller
     public function __construct() {
         $this->middleware('auth:api' , ['except' => ['login','register']]);
     }
-    public function register(Request $request)
+    public function register(UserRequest $request)
     {
-        
+        $request->validated();
     }
 
     public function login(Request $request)
