cb048220ee55979ab5c835ad5d0c98803f4c3309
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 18:31:35 +0430

    make user_id hidden

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,6 +11,7 @@ class Address extends Model
     use HasFactory;
 
     protected $fillable = ['user_id','title','address','latitude','longitude'];
+    protected $hidden = ['user_id'];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
