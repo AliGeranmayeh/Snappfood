b2c43c04bf4ed918c49305091762ef8d1ff5c14b
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 18:30:20 +0430

    create user address

--- app/Http/Controllers/api/ShopperAddressController.php
+++ app/Http/Controllers/api/ShopperAddressController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\api\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\AddressRequest;
+use App\Models\Address;
 
 class ShopperAddressController extends Controller
 {
-    public function add()
+    public function add(AddressRequest $request)
     {
-        
+      $address = Address::create($request->validated());
+      
+      return response()->json([
+          "message" => 'new address added',
+          'address' => $address
+      ],200);
     }
 }
