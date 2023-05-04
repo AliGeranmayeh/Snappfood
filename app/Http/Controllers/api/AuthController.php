6bf98becdc535a57d0d3d53ce9feeb48fa6e1123
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 13:36:46 +0430

    add simple register and login in auth controller

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -2,24 +2,43 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\api\Controller;
 use Illuminate\Http\Request;
 use Auth;
 use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('auth:api' , ['except' => ['login','register']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
     public function register(UserRequest $request)
     {
-        $request->validated();
+        
+        
+        $user = User::create(
+            // 'name' => $request['name'],
+            // 'email' => $request['email'],
+            // 'phone_number' => $request['phone_number'],
+            // 'role' => $request['role'],
+            // 'password' => Hash::make($request['password']),
+            $request->validated()
+        );
+
+        return response()->json([
+            'message' => 'User successfully registerd',
+            'user' => $user
+        ],201);
     }
 
     public function login(Request $request)
     {
-        
+        $creds = $request->only(['email', 'password']);
+        $token =auth()->attempt($creds);
+
+        return $token;
     }
 }
