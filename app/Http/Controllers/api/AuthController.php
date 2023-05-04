6d6ed4a7d05fea3013c7435f8e8c7bfafb81cea6
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 18:16:19 +0430

    Update AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,42 +3,60 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\api\Controller;
-use Illuminate\Http\Request;
-use Auth;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\JWTAuth;
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
 
-    public function __construct() {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
-    }
+    // public function __construct() {
+    //     $this->middleware('auth:api', ['except' => ['login','register']]);
+    // }
     public function register(UserRequest $request)
     {
-        
-        
-        $user = User::create(
-            // 'name' => $request['name'],
-            // 'email' => $request['email'],
-            // 'phone_number' => $request['phone_number'],
-            // 'role' => $request['role'],
-            // 'password' => Hash::make($request['password']),
-            $request->validated()
-        );
+        // dd($request->validated());
+        $user = User::create(array_merge(
+            $request->validated(),
+            ['password' => Hash::make($request->password)]
+        ));
 
+        $token = $user->createToken('myAppToken')->plainTextToken;
+    
         return response()->json([
-            'message' => 'User successfully registerd',
-            'user' => $user
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+            ]
         ],201);
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $creds = $request->only(['email', 'password']);
-        $token =auth()->attempt($creds);
 
-        return $token;
+        $user = User::where('email' , $request->email)->first();
+
+        if ( !$user || !Hash::check($request->password,$user->password)) {
+            return response()->json(['error' => 'bad credentials'], 401);
+        } 
+        $token = $user->createToken('myAppToken')->plainTextToken;
+        return response()->json([
+            'message' => 'Login was successful',
+            'token' => $token]
+            ,200);   
     }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'You are loged out'],200); 
+    }
+
 }
