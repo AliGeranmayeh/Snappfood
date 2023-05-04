75209247bad662ddb93b3edbf5858e323e38bb12
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 19:00:49 +0430

    complete add method

--- app/Http/Controllers/api/ShopperAddressController.php
+++ app/Http/Controllers/api/ShopperAddressController.php
@@ -12,19 +12,24 @@ class ShopperAddressController extends Controller
 {
     public function add(AddressRequest $request)
     {
+        if (Address::where('user_id', Auth::user()->id)->where('title', $request->title)->first()) {
+            return response()->json([
+                "message" => 'This address title already exist.', ], 406);
+        }
 
-    if (Address::where('user_id',Auth::user()->id)->where('title',$request->title)) {
-        return response()->json([
-            "message" => 'This address title already exist.',],406);
-    }
-    $address = Address::create(array_merge(
-        $request->validated(),
+        if ($request->status == 'set' && Address::where('user_id',Auth::user()->id)->where('status','set')->first()) {
+            Address::where('user_id',Auth::user()->id)->where('status','set')->update(['status'=>'unset']);
+        }
+        $address = Address::create(array_merge(
+            $request->validated(),
         ['user_id' => Auth::user()->id]
-    ));
-      
-    return response()->json([
-          "message" => 'new address added',
-          'address' => $address
-      ],200);
+        ));
+
+        return response()->json([
+            "message" => 'new address added',
+            'address' => $address
+        ], 200);
     }
+
+    
 }
