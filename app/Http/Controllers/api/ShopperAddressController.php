1b1daba5972107b3a8d80665c88e77d4515ed7b9
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 18:43:13 +0430

    make address title for each user unique

--- app/Http/Controllers/api/ShopperAddressController.php
+++ app/Http/Controllers/api/ShopperAddressController.php
@@ -6,14 +6,23 @@ use App\Http\Controllers\api\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddressRequest;
 use App\Models\Address;
+use Illuminate\Support\Facades\Auth;
 
 class ShopperAddressController extends Controller
 {
     public function add(AddressRequest $request)
     {
-      $address = Address::create($request->validated());
+
+    if (Address::where('user_id',Auth::user()->id)->where('title',$request->title)) {
+        return response()->json([
+            "message" => 'This address title already exist.',],406);
+    }
+    $address = Address::create(array_merge(
+        $request->validated(),
+        ['user_id' => Auth::user()->id]
+    ));
       
-      return response()->json([
+    return response()->json([
           "message" => 'new address added',
           'address' => $address
       ],200);
