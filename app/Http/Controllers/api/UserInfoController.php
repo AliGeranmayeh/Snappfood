242320d05b753b20db36ad10e51b17708207f3d5
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-04 22:28:27 +0430

    update user info api

--- app/Http/Controllers/api/UserInfoController.php
+++ app/Http/Controllers/api/UserInfoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\api\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UpdateUserInfoRequest;
 
 class UserInfoController extends Controller
 {
@@ -13,6 +14,14 @@ class UserInfoController extends Controller
     {
         return response()->json([
             'message' => User::find(Auth::user()->id)
+        ],200);
+    }
+
+    public function update(UpdateUserInfoRequest $request)
+    {
+        User::where('id' , Auth::user()->id)->update($request->validated());
+        return response()->json([
+            'message' => 'your information updated successfully'
         ],200);
     }
 }
