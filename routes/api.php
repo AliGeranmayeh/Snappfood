89f3e825973a77eb9afaa870f621f3f2ca5bd6c9
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-05 12:52:04 +0430

    make route search get

--- routes/api.php
+++ routes/api.php
@@ -31,5 +31,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
 Route::post('/register' , [AuthController::class, 'register'])->name('shopper-register');
 Route::post('/login' , [AuthController::class, 'login'])->name('shopper-login');
 Route::get('/restaurants/{restaurant_id}' , [RestaurantController::class, 'index']);
-Route::patch('/restaurants' , [RestaurantController::class, 'search']);
+Route::get('/restaurants' , [RestaurantController::class, 'search']);
 Route::get('/restaurants/{restaurant_id}/foods' , [RestaurantController::class, 'foods']);
