fe73a3033f07d62c948444c31ce51752e86d27a6
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-10 15:02:09 +0430

    update product_id field to food_id

--- app/Http/Requests/AddToCartRequest.php
+++ app/Http/Requests/AddToCartRequest.php
@@ -22,7 +22,7 @@ class AddToCartRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'product_id'=> 'required|numeric',
+            'food_id'=> 'required|numeric',
             'count' => 'required|numeric'
         ];
     }
