1916ce8dfa87da6f6ef1de7fb70616c104c3b365
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-11 19:14:54 +0430

    create simple view and controller for showing orders

--- app/Http/Controllers/Restaurant/OrderController.php
+++ app/Http/Controllers/Restaurant/OrderController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Restaurant;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function index()
-    {
-        return true;
+    { 
+        return view('restaurant_owner.orders-list',[
+            'orders' => Auth::user()->restaurant->orders
+        ]);
     }
 }
