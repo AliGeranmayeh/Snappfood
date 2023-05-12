da90551261725d0ddfe1b9dfdd11d331e319b8cc
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-12 08:25:24 +0430

    send email to costumer

--- app/Http/Controllers/Restaurant/OrderController.php
+++ app/Http/Controllers/Restaurant/OrderController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Cart;
+use App\Models\User;
+use App\Mail\SendMail;
 
 class OrderController extends Controller
 {
@@ -45,7 +49,15 @@ class OrderController extends Controller
     public function changeOrderStatus($order_status , $order_id)
     {
         if ($order_status != null) {
-            Order::where('id',$order_id)->update(['order_status'=>$order_status]); 
+            $cart_id= Auth::user()->restaurant->orders->where('id',$order_id)->first()->cart_id;
+            $user_id = Cart::find($cart_id)->user_id;
+            $ordered_email = User::find($user_id)->email;
+            Order::where('id',$order_id)->update(['order_status'=>$order_status]);
+
+            $mail_data = [
+                'content' => "your order status is $order_status" 
+            ];
+             Mail::to($ordered_email)->send(new SendMail($mail_data));
             // Auth::user()->restaurant->orders->where('id',$order_id)->update(['order_status'=>$order_status]);   
         }
         return redirect()->route('show-order-page');
