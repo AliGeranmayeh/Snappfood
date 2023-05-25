2ff6ff69baf811041327f588952544defe177a6c
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-25 21:05:46 +0430

    make status integer

--- database/migrations/2023_05_25_092653_create_comments_table.php
+++ database/migrations/2023_05_25_092653_create_comments_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->text('comment');
             $table->bigInteger('parent_id')->nullable();
-            $table->boolean('status'); //0=> waiting for confirmatio, 1=> confirmed, 2=> delete request
+            $table->integer('status'); //0=> waiting for confirmatio, 1=> confirmed, 2=> delete request
             $table->timestamps();
         });
     }
