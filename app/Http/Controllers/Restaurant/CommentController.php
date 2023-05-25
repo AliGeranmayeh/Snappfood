ba35a2474fcb536e089af20efdbffea3367f51da
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-25 21:21:12 +0430

    uncomment if statement

--- app/Http/Controllers/Restaurant/CommentController.php
+++ app/Http/Controllers/Restaurant/CommentController.php
@@ -17,13 +17,13 @@ class CommentController extends Controller
         $db_comments = Auth::user()->restaurant->comments;
 
         foreach ($db_comments as $db_comment) {
-            // if ($db_comment->status == 0) {
+            if ($db_comment->status == 0) {
                 $comments[] = [
                     'id' => $db_comment->id,
                     'user' => User::find($db_comment->user_id)->name,
                     'comment' => $db_comment->comment
                 ];
-            // }
+            }
         }
         return view('restaurant_owner.not_confirmed_comments' , [
             'comments' => $comments
