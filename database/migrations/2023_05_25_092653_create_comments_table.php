4552f587fd9d32eb98c68e929a89d8de0c2234b2
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-25 17:48:31 +0430

    add cart_id to comments table

--- database/migrations/2023_05_25_092653_create_comments_table.php
+++ database/migrations/2023_05_25_092653_create_comments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('restaurant_id')->constrained('restaurants', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('order_id')->constrained('orders', 'id')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('cart_id')->constrained('carts', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->text('comment');
             $table->bigInteger('parent_id')->nullable();
