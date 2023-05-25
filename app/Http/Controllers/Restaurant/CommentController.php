7cee1055a5a404a5f72785478e4f64a352ffcf4f
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-25 20:48:43 +0430

    add simple view and controller for showing unconfirmed comments

--- app/Http/Controllers/Restaurant/CommentController.php
+++ app/Http/Controllers/Restaurant/CommentController.php
@@ -6,12 +6,22 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\User;
 
 class CommentController extends Controller
 {
     public function index()
     {
-        $comments = Auth::user()->restaurant->comments;
+        $comments = [];
+        $db_comments = Auth::user()->restaurant->comments;
+
+        foreach ($db_comments as $db_comment) {
+            $comments[] = [
+                'id' => $db_comment->id,
+                'user' => User::find($db_comment->user_id)->name,
+                'comment' => $db_comment->comment
+            ];
+        }
         return view('restaurant_owner.not_confirmed_comments' , [
             'comments' => $comments
         ]);
--- resources/views/restaurant_owner/not_confirmed_comments.blade.php
+++ resources/views/restaurant_owner/not_confirmed_comments.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.main-nav')
+@section('css-link')
+    <style>
+        .text{
+            padding: 1%;
+            width:100%;
+        }
+        article{
+            width: 65%;
+            margin: auto
+        }
+    </style>
+@endsection
 
 @section('content')
-   @foreach ($comments as $comment)
-       
-   @endforeach
+    
+    @if (count($comments) == 0)
+        <h2 class="text-center text-white">No not  confirmed comments is available...</h2>
+    @endif
+    <div class="container grid my-5">
+        @foreach ($comments as $comment)
+            <article class="rounded " style="background-color: white">
+                <div  class="text">
+                    <h5 ><b>User: {{$comment['user']}}</b></h5>
+                    <div>
+                    <p style="padding:0 3%">{{$comment['comment']}}</p>
+                     <form action="" method="post" class="d-flex justify-content-center">
+                            @csrf
+                            <button name="delete" type="button" class="btn btn-danger m-1  w-25" value="{{$comment['id']}}"> Delete</button>
+                            <button name="confirm" type="button" class="btn btn-success m-1 w-25" value="{{$comment['id']}}">Confirm</button>
+                     </form>
+                    </div>
+                </div>
+            </article>
+        @endforeach
+    </div>
+    </div>
 @endsection
