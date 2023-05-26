51f4d97f2adbf084a74df8dc023bfcbbd973a5c4
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-26 11:50:42 +0430

    delete user_id from banner table

--- database/migrations/2023_05_26_070031_create_banners_table.php
+++ database/migrations/2023_05_26_070031_create_banners_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->text('text');
             $table->timestamps();
         });
