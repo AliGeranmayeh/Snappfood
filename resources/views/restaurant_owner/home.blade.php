a0f1b651388109b040f49fbb9048e60b002939a4
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-30 16:47:46 +0430

    update food cards style

--- resources/views/restaurant_owner/home.blade.php
+++ resources/views/restaurant_owner/home.blade.php
@@ -3,7 +3,7 @@
 
 @section('css-link')
     <style>
-        .grid {
+        .grid-1 {
             display: grid;
             grid-template-columns: repeat(auto-fill, minmax(200px, 2fr));
             grid-gap: 20px;
@@ -15,12 +15,12 @@
             box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
         }
 
-        .grid>article img {
-            max-width: 100%;
+        p {
+            font-size: 25px
         }
 
-        .text {
-            padding: 0 10px 10px;
+        .grid>article img {
+            max-width: 100%;
         }
     </style>
 @endsection
@@ -42,37 +42,46 @@
     @if (count($foods) == 0)
         <h2 class="text-center text-white">No food is available...</h2>
     @endif
-    <div class="container grid">
+    <div class="container grid-1">
         <div class="row row-cols-4 gap-3">
             @foreach ($foods as $food)
                 @if ($food->discount_id == $food_party_id)
-                    <article class="rounded p-3 mb-2 bg-secondary text-white">
+                    <article class="rounded  mb-2 bg-secondary text-white">
                     @else
-                        <article class="rounded " style="background-color: white">
+                        <article class="rounded p-0" style="background-color: white">
                 @endif
 
                 <img class="rounded-top" src="{{ asset($food->image) }}" alt="user-photo"
                     style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;width:100%">
-                <div class="text ">
-                    <h3 style="margin:10% 0"><b>{{ $food->name }}</b></h3>
+                <div class="p-3">
+                    <h3 style="margin:10% 0"><b>Food: {{ $food->name }}</b></h3>
                     <div>
                         @if ($food->materials)
-                            <p>Materials: {{ $food->materials }}</p>
+                            <p style="font-size: 18px">Materials: {{ $food->materials }}</p>
                         @endif
                         @if ($food->discount_id)
-                            <p>Price: <span style="text-decoration: line-through;"> {{ $food->price }}</span> -> with
+                            <p style="font-size: 18px">Price: <span
+                                    style="text-decoration: line-through;text-decoration-color:#fa4454;text-decoration-thickness: 2px;">
+                                    {{ $food->price }}</span> <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg> with
                                 {{ $food->discount * 100 }}% off
-                                <span>{{ $food->price - $food->price * $food->discount }}</span>
+                                <span style="color: #fa4454">{{ $food->price - $food->price * $food->discount }}</span> T
                             </p>
                         @else
-                            <p>Price: {{ $food->price }}</p>
+                            <p style="font-size: 18px">Price: {{ $food->price }} T</p>
                         @endif
-                        <form action="" method="post">
+
+                        <form action="" method="post" style="display: flex;justify-content:center">
                             @csrf
-                            <button name="delete" class="btn btn-danger" type="submit"
+                            <button name="delete" class="btn btn-danger w-50" type="submit" style="font-size: 16px" 
                                 value="{{ $food->id }}">Delete</button>
-                            <a href="edit_food/{{ $food->id }}" class="btn btn-warning mx-2">Edit</a>
+                            <a href="edit_food/{{ $food->id }}" class="btn btn-warning mx-2 w-50 text-white" style="font-size: 16px">Edit</a>
                         </form>
+
+
                     </div>
                     {{-- <a href="user/{{$user->id}}" class="btn btn-primary">More</a> --}}
 
