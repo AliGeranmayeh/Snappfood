1808486fe5e4f90380ad9e1a338e4296dc2c74e8
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-05-30 15:51:06 +0430

    add gap and limit for a row

--- resources/views/restaurant_owner/home.blade.php
+++ resources/views/restaurant_owner/home.blade.php
@@ -40,43 +40,45 @@
         <button class="btn btn-outline-light" type="submit" name="search">Search</button>
     </form>
     @if (count($foods) == 0)
-            <h2 class="text-center text-white">No food is available...</h2>
-        @endif
+        <h2 class="text-center text-white">No food is available...</h2>
+    @endif
     <div class="container grid">
-        @foreach ($foods as $food)
-            @if ($food->discount_id == $food_party_id)
-                <article class="rounded p-3 mb-2 bg-secondary text-white">
-                @else
-                    <article class="rounded " style="background-color: white">
-            @endif
-
-            <img class="rounded-top" src="{{ asset($food->image) }}" alt="user-photo"
-                style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;width:100%">
-            <div class="text ">
-                <h3 style="margin:10% 0"><b>{{ $food->name }}</b></h3>
-                <div>
-                    @if ($food->materials)
-                        <p>Materials: {{ $food->materials }}</p>
-                    @endif
-                    @if ($food->discount_id)
-                        <p>Price: <span style="text-decoration: line-through;"> {{ $food->price }}</span> -> with
-                            {{ $food->discount * 100 }}% off <span>{{ $food->price - $food->price * $food->discount }}</span>
-                        </p>
+        <div class="row row-cols-4 gap-3">
+            @foreach ($foods as $food)
+                @if ($food->discount_id == $food_party_id)
+                    <article class="rounded p-3 mb-2 bg-secondary text-white">
                     @else
-                        <p>Price: {{ $food->price }}</p>
-                    @endif
-                    <form action="" method="post">
-                        @csrf
-                        <button name="delete" class="btn btn-danger" type="submit"
-                            value="{{ $food->id }}">Delete</button>
-                        <a href="edit_food/{{ $food->id }}" class="btn btn-warning mx-2">Edit</a>
-                    </form>
-                </div>
-                {{-- <a href="user/{{$user->id}}" class="btn btn-primary">More</a> --}}
+                        <article class="rounded " style="background-color: white">
+                @endif
 
-            </div>
-            </article>
-        @endforeach
-    </div>
+                <img class="rounded-top" src="{{ asset($food->image) }}" alt="user-photo"
+                    style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;width:100%">
+                <div class="text ">
+                    <h3 style="margin:10% 0"><b>{{ $food->name }}</b></h3>
+                    <div>
+                        @if ($food->materials)
+                            <p>Materials: {{ $food->materials }}</p>
+                        @endif
+                        @if ($food->discount_id)
+                            <p>Price: <span style="text-decoration: line-through;"> {{ $food->price }}</span> -> with
+                                {{ $food->discount * 100 }}% off
+                                <span>{{ $food->price - $food->price * $food->discount }}</span>
+                            </p>
+                        @else
+                            <p>Price: {{ $food->price }}</p>
+                        @endif
+                        <form action="" method="post">
+                            @csrf
+                            <button name="delete" class="btn btn-danger" type="submit"
+                                value="{{ $food->id }}">Delete</button>
+                            <a href="edit_food/{{ $food->id }}" class="btn btn-warning mx-2">Edit</a>
+                        </form>
+                    </div>
+                    {{-- <a href="user/{{$user->id}}" class="btn btn-primary">More</a> --}}
+
+                </div>
+                </article>
+            @endforeach
+        </div>
     </div>
 @endsection
