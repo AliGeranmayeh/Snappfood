2e5da9700171b90ad1c178dbcc2c0046d68378e7
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-06-01 22:46:59 +0430

    update food categories style

--- resources/views/admin/food-category.blade.php
+++ resources/views/admin/food-category.blade.php
@@ -6,6 +6,39 @@
                 <strong class="d-flex justify-content-center">{{$error}}</strong>
             </div>
         @endif
+        @if ($name != null)
+            <div class="page rounded-3 bg-white py-3 px-4 mb-5"
+                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+                <h3 class="text-center">Edit Food Category</h3>
+                <div class="my-5">
+                    <form method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Food Category Name</label>
+                            <input value="{{ $name }}" type="text" class="form-control " id="name"
+                                name="name">
+                        </div>
+                        <button value="{{ $id }}" name="update" type="submit"
+                            class="btn btn-primary my-2">Update</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <div class="page rounded-3 bg-white py-3 px-4 mb-5"
+                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+                <h3 class="text-center">Add New Food Category</h3>
+                <div class="my-5">
+                    <form method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Food Category Name</label>
+                            <input type="text" class="form-control " id="name" name="name" required>
+                        </div>
+                        <button name="create" type="submit" class="btn btn-primary my-2">Create</button>
+                    </form>
+                </div>
+            </div>
+        @endif
         <div class="page rounded-3 bg-white p-3 "
             style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
             <h3 class="text-center">Existed Food Categories</h3>
@@ -13,13 +46,14 @@
                 @if (count($food_categories) == 0)
                 <h4 class="text-center">There is no food category...</h4>
                 @endif
+                
                 <table class="table">
                     <tbody>
                         @foreach ($food_categories as $key => $food_category)
                             <tr>
                                 <th scope="row">
                                     @php $row_number = $key; @endphp
-                                    {{ $row_number++ }}
+                                    {{ ++$row_number }}
                                 </th>
                                 <td>{{ $food_category->name }}</td>
                                 <td class="d-flex flex-row-reverse">
@@ -40,38 +74,6 @@
                 </table>
             </div>
         </div>
-        @if ($name != null)
-            <div class="page rounded-3 bg-white py-3 px-4 my-5"
-                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-                <h3 class="text-center">Edit Food Category</h3>
-                <div class="my-5">
-                    <form method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Food Category Name</label>
-                            <input value="{{ $name }}" type="text" class="form-control " id="name"
-                                name="name">
-                        </div>
-                        <button value="{{ $id }}" name="update" type="submit"
-                            class="btn btn-primary my-2">Update</button>
-                    </form>
-                </div>
-            </div>
-        @else
-            <div class="page rounded-3 bg-white py-3 px-4 my-5"
-                style="margin:auto; width: 50%;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-                <h3 class="text-center">Add New Food Category</h3>
-                <div class="my-5">
-                    <form method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Food Category Name</label>
-                            <input type="text" class="form-control " id="name" name="name" required>
-                        </div>
-                        <button name="create" type="submit" class="btn btn-primary my-2">Create</button>
-                    </form>
-                </div>
-            </div>
-        @endif
+        
     </div>
 @endsection
