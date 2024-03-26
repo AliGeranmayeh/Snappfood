f0e8e9703fb471361d872d3faf5300f0376a0060
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-03-27 00:24:03 +0430

    use order status enum in orders table

--- database/migrations/2023_05_11_134859_create_orders_table.php
+++ database/migrations/2023_05_11_134859_create_orders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\OrderStatusEnum;
 
 return new class extends Migration
 {
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('cart_id')->constrained('carts', 'id')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('restaurant_id')->constrained('restaurants', 'id')->onUpdate('cascade')->onDelete('cascade');
-            $table->enum('order_status', ['checking','preparing','sending','delivered'])->default('checking');/*checking->preparing->sending->delivered */
+            $table->enum('order_status', ['checking','preparing','sending','delivered'])->default(OrderStatusEnum::CHECKING->value);
             $table->timestamps();
         });
     }
